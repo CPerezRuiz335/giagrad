c5da7f45c1e33154ca2e1268a7bd0c06df2f366d
CPerezRuiz335 <carlosp335@gmail.com>
2023-03-28 19:52:26 +0200

    lets try sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,9 +19,8 @@ extensions =  [
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
 ]
+
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -33,10 +32,3 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'pydata_sphinx_theme'
 html_static_path = ['_static']
-
-
-# -- Extension configuration -------------------------------------------------
-
-# add sourcecode to path
-import sys, os
-sys.path.insert(0, os.path.abspath('../giagrad'))
